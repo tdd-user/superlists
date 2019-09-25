baf00d521212bac2abf0cb63b24a88da908bce43
tdd-user <akties2016@yahoo.com>
2019-09-25 15:35:02 +0200

    fixed copy paste error test sucesful: AssertionError: 'To-Do' not found in 'Welcome to Django'

--- functional_tests.py
+++ functional_tests.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 
 import unittest
 
@@ -19,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
         self.fail('Finish the test!')  
 
         # She is invited to enter a to-do item straight away
-        [...rest of comments as before]
+        #[...rest of comments as before]
 
 if __name__ == '__main__':  
     unittest.main(warnings='ignore')  
