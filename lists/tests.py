a29e642829cfb2fe2d9918d6faa5576d2169d747
tdd-user <akties2016@yahoo.com>
2019-09-26 15:58:41 +0200

    adding code to unit test

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,7 @@
 from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
-
+from django.http import HttpRequest
 
 class HomePageTest(TestCase):
 
@@ -11,4 +11,10 @@ class HomePageTest(TestCase):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
         
-        
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home_page(request)  
+        html = response.content.decode('utf8')  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>To-Do lists</title>', html)  
+        self.assertTrue(html.endswith('</html>'))  
