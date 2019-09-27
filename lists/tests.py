f8b479abe727bf70e273439d4bf8741988d2cc85
tdd-user <akties2016@yahoo.com>
2019-09-27 23:40:45 +0200

    clean up

--- lists/tests.py
+++ lists/tests.py
@@ -10,6 +10,7 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+       
         
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  
