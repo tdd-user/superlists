ea7b23b24caf37c2cf83506ddbd95e365e82ede7
tdd-user <akties2016@yahoo.com>
2019-09-29 17:29:19 +0200

    back to 5.4

--- functional_tests.py
+++ functional_tests.py
@@ -49,11 +49,10 @@ class NewVisitorTest(unittest.TestCase):
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
         inputbox = self.browser.find_element_by_id('id_new_item')  
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         inputbox.send_keys('Use peacock feathers to make a fly')  
         inputbox.send_keys(Keys.ENTER)  
-        time.sleep(3) 
+        time.sleep(1) 
      
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table('1: Buy peacock feathers')
