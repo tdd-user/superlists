8e00fbe5e9d75d65835b2f0c95166b60e0fc3c68
tdd-user <akties2016@yahoo.com>
2019-09-29 13:15:32 +0200

    second list item test

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -31,33 +36,33 @@ class NewVisitorTest(unittest.TestCase):
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')  
-
+        time.sleep(1) 
+                
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)  
         time.sleep(1)  
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows), "New To Do item did not appear in table."
-            )
-
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
-        self.fail('Finish the test!')
-
+        inputbox = self.browser.find_element_by_id('id_new_item')  
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        inputbox.send_keys('Use peacock feathers to make a fly')  
+        time.sleep(3) 
+     
         # The page updates again, and now shows both items on her list
-
-        # The page updates again, and now shows both items on her list
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-
+        self.fail('Finish the test!')
+        
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
--- lists/tests.py
+++ lists/tests.py
@@ -5,20 +5,32 @@ from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 
+#from pdb import set_trace
+#set_trace() 
+# pause for a moment to check whatever values
+
 class HomePageTest(TestCase):
 
+    
+        
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
        
         
-    def test_home_page_returns_correct_html(self):
-
+    def test_home_page_request_returns_correct_html(self):
         response = self.client.get('/')  
         html = response.content.decode('utf8')  
+        
         self.assertTrue(html.strip().startswith('<html>'), "check the home page's html tags")  
         self.assertIn('<title>To-Do lists</title>', html)  
         self.assertTrue(html.strip().endswith('</html>'), "check the home page's html tags")  
-        print(repr(html))
+        #print(repr(html))
         self.assertTemplateUsed(response, 'home.html')  
         
+    def test_user_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        html = response.content.decode('utf8')  
+        #print(repr(html))
+    
