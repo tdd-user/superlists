ff77c9591a9401f402790cdb8e8b9bbb417b442e
tdd-user <akties2016@yahoo.com>
2019-09-29 17:25:41 +0200

    back to 5.4

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,6 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
-
 from lists.models import Item
 
 #from pdb import set_trace
@@ -19,17 +18,16 @@ class HomePageTest(TestCase):
         
     def test_home_page_request_returns_correct_html(self):
         response = self.client.get('/')  
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.strip().startswith('<html>'), "check the home page's html tags")  
-        self.assertIn('<title>To-Do lists</title>', html)  
-        self.assertTrue(html.strip().endswith('</html>'), "check the home page's html tags")  
+        #html = response.content.decode('utf8')  
         #print(repr(html))
         self.assertTemplateUsed(response, 'home.html')  
         
     def test_user_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        html = response.content.decode('utf8')  
+        self.assertEqual(Item.objects.count(), 1)  
+        new_item = Item.objects.first()  
+        self.assertEqual(new_item.text, 'A new list item')  
+        #html = response.content.decode('utf8')  
         #print(repr(html))
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,13 @@
-#from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from lists.models import Item
+
 # Create your views here.
 def home_page(request):
-    return render(request, 'home.html',{ 'new_item_text': request.POST.get('item_text', ''),})
+
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+    
+    return render(request, 'home.html', { 'new_item_text': request.POST.get('item_text',''), })
         
